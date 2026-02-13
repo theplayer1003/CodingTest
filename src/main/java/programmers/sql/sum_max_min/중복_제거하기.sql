72ee4d8cf219d6cc9150d3c0c09786b88d99b06e
김준기 <theplayer1003@MacBook-Pro.local>
2026-02-13 14:18:35 +0900

    prgrammers_sql_smm_중복 제거하기
    - DISTINCT
    - 함수와 키워드 구분

--- src/main/java/programmers/sql/sum_max_min/중복_제거하기.sql
+++ src/main/java/programmers/sql/sum_max_min/중복_제거하기.sql
@@ -1,5 +1,26 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59408
 
-SELECT COUNT(DISTINCT(NAME)) AS COUNT
+SELECT COUNT(DISTINCT (NAME)) AS COUNT
 FROM ANIMAL_INS
-ㅇ
+
+
+SELECT COUNT(DISTINCT NAME) AS COUNT
+FROM ANIMAL_INS
+
+/*
+ DISTINCT 는 함수가 아니라 키워드 다.
+ 함수처럼 괄호로 묶어도 알아서 작동하지만 본래 문법대로 중복 제거를 지시하는 수식어로 쓰는게 표준이다.
+
+ 함수: 값을 받아 변환하는 기계
+ UPPER('abc') -> 'ABC'
+ SUBSTR('hello', 1, 2) -> 'he'
+ ROUND(3.14519, 2) -> 3.14
+
+ COUNT(ID)
+ SUM(PRICE)
+ MAX(DATE)
+
+ 키워드: 문법을 지시하는 명령어
+ SELECT, FROM, WHERE, GROUP BY, ORDER BY
+ AND, OR, AS, IN, IS NULL1, DISTINT
+ */
